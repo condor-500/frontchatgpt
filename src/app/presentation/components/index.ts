245f8a2c6e3dd005807f13c3ab64f8e99a06bfb6
jjandres12 <jjandres12@gmail.com>
2024-03-07 03:10:18 +0000

    agrera burbujas

---
+++ src/app/presentation/components/index.ts
@@ -0,0 +1,4 @@
+export * from './text-boxes/textMessageBox/textMessageBox.component';
+export * from './chat-bubbles/chatMessage/chatMessage.component';
+export * from './typingLoader/typingLoader.component';
+export * from './text-boxes/textMessageBoxFile/textMessageBoxFile.component';
--- src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
+++ src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
@@ -1,8 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ChatMessageComponent } from '@components/chat-bubbles/chatMessage/chatMessage.component';
+
 import { MyMessageComponent } from '@components/chat-bubbles/myMessage/myMessage.component';
-import { TypingLoaderComponent } from '@components/typingLoader/typingLoader.component';
+import { ChatMessageComponent, TextMessageBoxFileComponent, TypingLoaderComponent } from '@components/index';
+import { TextMessageBoxComponent } from '@components/text-boxes/textMessageBox/textMessageBox.component';
+
+
 
 
 @Component({
@@ -12,9 +15,16 @@ import { TypingLoaderComponent } from '@components/typingLoader/typingLoader.com
     CommonModule,
     ChatMessageComponent,
     MyMessageComponent,
-    TypingLoaderComponent
+    TypingLoaderComponent,
+    TextMessageBoxComponent,
+    TextMessageBoxFileComponent
   ],
   templateUrl: './orthographyPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class OrthographyPageComponent { }
+export default class OrthographyPageComponent {
+
+  handleMessage(promt:string){
+    console.log(promt)
+  }
+ }
